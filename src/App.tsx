61e7e3602d55a87efbb6f016d07d6d7cf1f014d7
sugarfrosted <sugarfrosted2005@gmail.com>
2023-11-15 15:02:47 -0600

    cleaned up state

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DogApiSession from './api/data/DogApi';
-import { AppBar, Toolbar, IconButton, Typography, Button } from '@mui/material';
-import { Menu } from '@mui/icons-material'
 
 function App() {
-
   return (
     <div className="App">
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
