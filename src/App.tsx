715aa41285ad2dc80becb5a41240045c5dbc4499
sugarfrosted <sugarfrosted2005@gmail.com>
2023-11-15 18:28:11 -0600

    Add topbar control

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DogApiSession from './api/data/DogApi';
+import { Box, AppBar, Toolbar, Typography, Button, IconButton, TextField } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Box><TextField id="tfAuthName" label="Name"/> <TextField id="tfAuthEmail"/></Box>
+    </Box>
   );
 }
 
