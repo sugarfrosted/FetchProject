e97b697ee4432da3fcce5ffdbbf3146d424742db
sugarfrosted <sugarfrosted2005@gmail.com>
2023-12-10 16:50:48 -0600

    Code reorganization

--- src/components/DogSearchResults/DogMatchDisplay.tsx
+++ src/components/DogSearchResults/DogMatchDisplay.tsx
@@ -40,41 +40,58 @@ const Transition = forwardRef(function Transition(
 });
 
 export default function DogMatchDisplay(props: DogMatchDisplayProps) {
+    return (
+    /* eslint-disable indent */
+      <>
+        {
+          props.match ? (
+            <DogMatchPopup {...props} />
+          ) : (
+            <NoMatchFound {...props} />
+          )
+        }
+      </>
+    /* eslint-enable indent */
+    );
+}
+
+export function DogMatchPopup(props: DogMatchDisplayProps) {
 
     const displayAge = useMemo(() => PrettifyAge(props.match?.age), [props]);
+
+    if(!props.match) {
+        return <></>;
+    }
 
     return (
     /* eslint-disable indent */
       <Dialog sx={props.sx} open={!!props.match && props.open} TransitionComponent={Transition}>
-        {props.match ? (
-          <Card title={props.match.name}>
-            <CardHeader title={props.match.name} subheader="Your new best friend!" />
-            <CardMedia
-              component="img"
-              alt="The house from the offer."
-              src={props.match.img} />
-            <CardContent>
-              <Stack direction="row" flexDirection="row">
-                <Box flex={1}><label id="matchBreed" >Breed:</label><span aria-labelledby="matchBreed">{props.match.breed}</span></Box>
-                <Box flex={1}><label id="matchAge">Age:</label><span aria-labelledby="matchAge">{displayAge}</span></Box>
-                <Box flex={1}><label id="matchZip" >Location:</label><span aria-labelledby="matchZip">{props.match.zip_code}</span></Box>
-              </Stack>
-            </CardContent>
-            <CardActions>
-              <Button onClick={props.onClose}>Search for another dog</Button>
-            </CardActions>
-          </Card>) :
-          null
-        }
+        <Card title={props.match.name}>
+          <CardHeader title={props.match.name} subheader="Your new best friend!" />
+          <CardMedia
+            component="img"
+            alt="The house from the offer."
+            src={props.match.img} />
+          <CardContent>
+            <Stack direction="row" flexDirection="row">
+              <Box flex={1}><label id="matchBreed" >Breed:</label><span aria-labelledby="matchBreed">{props.match.breed}</span></Box>
+              <Box flex={1}><label id="matchAge">Age:</label><span aria-labelledby="matchAge">{displayAge}</span></Box>
+              <Box flex={1}><label id="matchZip" >Location:</label><span aria-labelledby="matchZip">{props.match.zip_code}</span></Box>
+            </Stack>
+          </CardContent>
+          <CardActions>
+            <Button id={'btnCloseSearchResult' as DogMatchButtonIds} onClick={props.onClose}>Search for another dog</Button>
+          </CardActions>
+        </Card>)
       </Dialog>
     /* eslint-enable indent */
     );
 }
 
-export function NoMatchFound(props: {open: boolean, handleClose: () => void}){
+export function NoMatchFound(props: PopupProps){
     return (
     /* eslint-disable indent */
-      <Dialog open={props.open} onClose={props.handleClose}
+      <Dialog sx={props.sx} open={props.open} onClose={props.onClose}
         TransitionComponent={Transition}
         keepMounted
         aria-describedby="alert-no-match-dialogdescription" >
@@ -85,16 +102,21 @@ export function NoMatchFound(props: {open: boolean, handleClose: () => void}){
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={props.handleClose}>Ok</Button>
+          <Button id={'btnCloseNoMatchFound' as DogMatchButtonIds} onClick={props.onClose}>Ok</Button>
         </DialogActions>
       </Dialog>
     /* eslint-enable indent */
     );
 }
 
-interface DogMatchDisplayProps {
+export type DogMatchButtonIds = 'btnCloseNoMatchFound' | 'btnCloseSearchResult';
+
+interface DogMatchDisplayProps extends PopupProps {
     match: Dog | undefined;
-    open: boolean;
+}
+
+interface PopupProps {
     sx?: SxProps<Theme> | undefined;
-    onClose?: () => void;
+    open: boolean;
+    onClose?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
