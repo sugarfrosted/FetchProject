94b36a78ee71dba3c19aecdd44e4004bc15bc763
sugarfrosted <sugarfrosted2005@gmail.com>
2023-12-11 16:32:15 -0600

    Checkpoint for experimentation

--- src/components/DogSearchResults/DogLocationSearch.tsx
+++ src/components/DogSearchResults/DogLocationSearch.tsx
@@ -3,33 +3,68 @@ import {
     UsStates,
 } from "../../api/data/LocationData";
 import {
+  Box,
+  Chip,
     FormControl,
     MenuItem,
+    MenuProps,
     Select,
+    SelectChangeEvent,
+    SxProps,
+    Theme,
 } from "@mui/material";
 import {
     InputLabel,
     ListSubheader,
 } from "@mui/material";
+import { ReactNode } from "react";
 
-export default function StateAndCityCriteria() {
+export default function StateAndCityCriteria(props: DogBreedDropdownProps) {
+    function renderBreeds(): React.ReactNode {
+        return (
+        /* eslint-disable indent */
+          <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 0.5 }}>
+            {props.selectedBreeds.map(value => (
+              <Chip key={value} label={value}/>
+            ))}
+          </Box>
+        /* eslint-enable indent */
+        );
+    };
+
     return (
     /* eslint-disable indent */
       <div>
-        <FormControl sx={{ m: 1, minWidth: 120 }}>
-          <InputLabel htmlFor="grouped-select">Grouping</InputLabel>
-          <Select defaultValue="" id="grouped-select" label="Grouping">
+        <FormControl sx={props.sx}>
+          <InputLabel htmlFor="selSelectStates">States and Provinces</InputLabel>
+          <Select
+            multiple
+            id="selSelectStates"
+            value={props.selectedBreeds}
+            onChange={props.handleChange}
+            label="States and Provinces"
+            renderValue={renderBreeds}
+            MenuProps={props.menuProps}
+          >
             <ListSubheader>{UsStates.name}</ListSubheader>
             {
-              UsStates.divisions.map(state => <MenuItem key={state.abbr}>{state.name}</MenuItem>)
+              UsStates.divisions.map(state => <MenuItem value={[state.abbr, state.name]} key={state.abbr}>{state.name}</MenuItem>)
             }
             <ListSubheader>{CaProvinces.name}</ListSubheader>
             {
-              CaProvinces.divisions.map(provinces => <MenuItem key={provinces.abbr}>{provinces.name}</MenuItem>)
+              CaProvinces.divisions.map(province => <MenuItem value={[province.abbr, province.name]} key={province.abbr}>{province.name}</MenuItem>)
             }
           </Select>
         </FormControl>
       </div>
     /* eslint-enable indent */
     );
+}
+
+interface DogBreedDropdownProps {
+    dogBreeds: string[]
+    selectedBreeds: string[];
+    menuProps?: Partial<MenuProps> | undefined;
+    sx?: SxProps<Theme> | undefined;
+    handleChange?: ((event: SelectChangeEvent<string[]>, child: ReactNode) => void) | undefined
 }
