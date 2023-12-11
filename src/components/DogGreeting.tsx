9b84c5ea7fc79a45cfb4152c0861d0dea17b8f4c
sugarfrosted <sugarfrosted2005@gmail.com>
2023-12-10 22:58:54 -0600

    Small style fixes

--- src/components/DogGreeting.tsx
+++ src/components/DogGreeting.tsx
@@ -1,3 +1,16 @@
+import {
+    Typography,
+} from "@mui/material";
+
 export default function DogGreeting() {
-    return <div>Welcome to Doggocom. You can do anything at Doggocom</div>;
+    return (
+    /* eslint-disable indent */
+      <div>
+        <Typography variant="h1">Welcome to Doggocom</Typography>
+        <Typography variant="h2">You can do anything at Doggocom</Typography>
+        <Typography variant="h3">Login to use the dog matching service</Typography>
+      </div>
+    /* eslint-enable indent */
+    );
+
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+/* eslint-disable indent */
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+/* eslint-enable indent */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
